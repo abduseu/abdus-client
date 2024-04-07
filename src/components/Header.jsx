a8f4ef88039c82cd4b163160d473fb08d4fc4d44
sal400 <flarebd@gmail.com>
2024-04-08 04:49:52 +0600

    Header logo Link fixt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar, MobileNav, Typography, IconButton } from "@material-tailwind/react";
 import { SiArtixlinux } from "react-icons/si";
 import {ReactTyped} from 'react-typed';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     const [openNav, setOpenNav] = React.useState(false);
@@ -34,9 +35,9 @@ const Header = () => {
         <div>
             <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
                 <div className="xl:container mx-auto flex items-center justify-between text-blue-gray-900">
-                    <Typography as="a" href="/" className="mr-4 cursor-pointer py-1.5 font-medium uppercase text-xl flex items-center gap-2">
+                    <Link to={'/'} className="mr-4 cursor-pointer py-1.5 font-medium uppercase text-xl flex items-center gap-2">
                         <SiArtixlinux /> Abdus Salam
-                    </Typography>
+                    </Link>
                     <div className="flex items-center gap-4">
                         <div className="mr-4 hidden lg:block">{navList}</div>
                         <IconButton variant="text" ripple={false} onClick={() => setOpenNav(!openNav)} className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden">
