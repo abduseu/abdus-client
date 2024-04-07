38309348d7c73eb4483a2df185938a3140f3d73d
sal400 <flarebd@gmail.com>
2024-04-07 18:08:50 +0600

    Banner section & footer UI updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Navbar, MobileNav, Typography, IconButton, Card } from "@material-tailwind/react";
+import { Navbar, MobileNav, Typography, IconButton } from "@material-tailwind/react";
 import { SiArtixlinux } from "react-icons/si";
-import { Link } from "react-router-dom";
+import {ReactTyped} from 'react-typed';
 
 const Header = () => {
     const [openNav, setOpenNav] = React.useState(false);
@@ -16,16 +16,16 @@ const Header = () => {
     const navList = (
         <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
             <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
-                <Link href="#about" className="flex items-center">About</Link>
+                <a href="#about" className="flex items-center">About</a>
             </Typography>
             <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
-                <Link href="#skills" className="flex items-center">Skills</Link>
+                <a href="#skills" className="flex items-center">Skills</a>
             </Typography>
             <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
-                <Link href="#portfolio" className="flex items-center">Portfolio</Link>
+                <a href="#portfolio" className="flex items-center">Portfolio</a>
             </Typography>
             <Typography as="li" variant="small" color="blue-gray" className="p-1 font-normal">
-                <Link href="#contact" className="flex items-center">Contact</Link>
+                <a href="#contact" className="flex items-center">Contact</a>
             </Typography>
         </ul>
     );
@@ -56,32 +56,13 @@ const Header = () => {
                     {navList}
                 </MobileNav>
             </Navbar>
-            <div className="mx-auto max-w-screen-md py-12">
-                <Card className="mb-12 overflow-hidden">
-                    <img
-                        alt="nature"
-                        className="h-[32rem] w-full object-cover object-center"
-                        src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
-                    />
-                </Card>
-                <Typography variant="h2" color="blue-gray" className="mb-2">
-                    What is Material Tailwind
-                </Typography>
-                <Typography color="gray" className="font-normal">
-                    Can you help me out? you will get a lot of free exposure doing this
-                    can my website be in english?. There is too much white space do less
-                    with more, so that will be a conversation piece can you rework to make
-                    the pizza look more delicious other agencies charge much lesser can
-                    you make the blue bluer?. I think we need to start from scratch can my
-                    website be in english?, yet make it sexy i&apos;ll pay you in a week
-                    we don&apos;t need to pay upfront i hope you understand can you make
-                    it stand out more?. Make the font bigger can you help me out? you will
-                    get a lot of free exposure doing this that&apos;s going to be a chunk
-                    of change other agencies charge much lesser. Are you busy this
-                    weekend? I have a new project with a tight deadline that&apos;s going
-                    to be a chunk of change. There are more projects lined up charge extra
-                    the next time.
-                </Typography>
+
+            {/* Banner Section */}
+            <div className=" mx-auto pb-12 text-white bg-banner">
+                <div className="flex flex-col justify-center items-center h-screen">
+                    <h3 className="md:text-3xl font-light pb-4">I'm Abdus Salam</h3>
+                    <h2 className="text-3xl md:text-6xl font-bold"><ReactTyped strings={["Frontend Developer", "Software Developer"]} typeSpeed={100} backSpeed={50} loop /></h2>
+                </div>
             </div>
         </div>
     );
