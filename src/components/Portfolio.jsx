31b05c100ac1a38e63715cb634f3e4b94ca731a7
sal400 <flarebd@gmail.com>
2024-04-12 01:08:06 +0600

    portfolio space-y-6 removed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,19 +7,19 @@ const Portfolio = () => {
                 </h1>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-center">
                     <a href="https://grassroot-restaurant.web.app" target="_blank" rel="noopener noreferrer">
-                        <div className="bg-white p-8 h-full space-y-2">
+                        <div className="bg-white p-8 h-full">
                             <img src="https://i.ibb.co/VVrjP1P/grassroot.png" className="h-48 mx-auto object-contain" />
                             <h3>Grassroot - Restaurant Management System</h3>
                         </div>
                     </a>
                     <a href="https://swissrealty-b5ce9.web.app" target="_blank" rel="noopener noreferrer">
-                        <div className="bg-white p-8 h-full space-y-2">
+                        <div className="bg-white p-8 h-full">
                             <img src="https://i.ibb.co/VWs0FD7/swissrealty.png" className="h-48 mx-auto object-contain" />
                             <h3>Swiss Realty - Real Estate Management Platform</h3>
                         </div>
                     </a>
                     <a href="https://studypeer-44518.web.app" target="_blank" rel="noopener noreferrer">
-                        <div className="bg-white p-8 h-full space-y-2">
+                        <div className="bg-white p-8 h-full">
                             <img src="https://i.ibb.co/HNCBHwx/studypeer.png" className="h-48 mx-auto object-contain" />
                             <h3>Studypeer - Peer based assignment marking system</h3>
                         </div>
